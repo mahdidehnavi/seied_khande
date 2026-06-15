205c323025356a236784ee3cfd2e5a5af1c4cecf
*mahdi* <*mahdidehnavi0086@gmail.com*>
2026-06-16 02:23:40 +0330

    fix : line 34 form function ability 1 int taha_k --> for ---> if

--- src/taha_k.cpp
+++ src/taha_k.cpp
@@ -29,7 +29,8 @@ void taha_k::ability1(Hero* ptTeam[] , Hero* ptEnemy[])
     int enemy;
     cout << "choose a player to damage(1-3): ";
     cin >> enemy;
-    for(enemy < 0 || enemy > 3){
+    if(enemy < 0 || enemy > 3)
+    {
         cout << "\nEnter correct number(1-3): ";
         cin >> enemy;
     }
